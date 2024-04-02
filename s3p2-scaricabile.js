72a00ff313bd746ca4228587db0bd42b0ad4a9d0
patriziapatti <patrizia.patti5@gmail.com>
2024-04-02 21:13:17 +0100

    correzioni finali

--- s3p2-scaricabile.js
+++ s3p2-scaricabile.js
@@ -39,9 +39,9 @@ function modificaIndirizzo(){
 
 // ESERCIZIO 10
 function aggiungiClasse (){
-  let nuovaClasse = document.querySelectorAll('p')
+  let nuovaClasse = document.querySelectorAll('a')
   for (i=0; i<nuovaClasse.length; i++)
-  nuovaClasse[i].classList.add ('nuovoStile') //a ciascun ciclo viene aggiunta la classe 'nuovo stile' ad ognuno dei 'p'
+  nuovaClasse[i].classList.add ('nuovoStile') //a ciascun ciclo viene aggiunta la classe 'nuovo stile' ad ognuno dei 'a'
 }
 // ho fatto mille ricerche per riuscire a eseguire correttamente questa funzione, non sono sicura di averne capito il senso
 
@@ -53,7 +53,28 @@ function aggiungiClasse (){
  }
 
 // ESERCIZIO 12 (ho dovuto fare due funzioni)
-function coloriRandom (num1){ //creo una funzione che mi ritorni dei numeri
+
+
+function coloriRandom2 (){
+  let random1 = Math.floor (Math.random()*255) //random 1 mi da un numero casuale da 0 a 255
+  let random2 = Math.floor (Math.random()*255) //random 2 mi da un numero casuale da 0 a 255
+  let random3 = Math.floor (Math.random()*255) //random 3 mi da un numero casuale da 0 a 255
+  return 'rgb(' + random1 +', ' + random2 + ', ' + random3 + ')'
+}
+
+
+
+ function colorePrezzo (){
+ let prezzi = document.querySelectorAll ('.prz') //richiamo la classe che ho assegnato nell'html - dentro prezzi avrò un array (0 è il primo dei prezzi, 1 è il secondo ecc, ecc)
+ for (i=0; i<prezzi.length; i++){ // ad ogni giro del ciclo prendo un prezzo 
+    let nuovoColore = coloriRandom2() // creo un contenitore che avrà il valore di quello che mi restituisce la  funzione coloriRandom2 che in questo caso è una stringa (rgb ecc ecc)
+    prezzi [i].style.color = nuovoColore // dall'array prezzi prendo l'i esimo elemento e accedo alla sua proprietà/attributo syle e colore e gli assegno il valore del nuovoColore
+ }
+}
+
+
+/* prima prova
+function coloriRandom (num1){ //creo una funzione che mi ritorni dei numeri 
   let arrayColori = [] //creo un array vuoto dove farò finire 3 numeri
   for (let i=0; i<num1; i++){
   let random1 = Math.floor (Math.random()*255) //random 1 mi da un numero casuale da 0 a 255
@@ -62,11 +83,4 @@ function coloriRandom (num1){ //creo una funzione che mi ritorni dei numeri
   arrayColori.push ('rgb(' + random1 +', ' + random2 + ', ' + random3 + ')') //pusho i 3 colori nell'array vuoto mettendo davanti rgb e le parentesi tonde con i numeri randomici all'interno 
 }
  return arrayColori //ritorno l'array di colori con i numeri randomici all'interno
-}
- function colorePrezzo (){
- let prezzi = document.querySelectorAll ('.prz') //richiamo la classe che ho assegnato nell'html
- let colori = coloriRandom(prezzi.length) //creo un array di colori con la stessa lunghezza di prezzi
- for (i=0; i<prezzi.length; i++){
- prezzi [i].style.color = colori[i] //do a ciascun elemento con classe prz un colore random
- }
-}
+}*/
